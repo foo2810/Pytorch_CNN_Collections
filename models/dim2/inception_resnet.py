63768de2e8a112a741a0a296f080e15a399213c7
foo2810 <ntshp.ak@gmail.com>
2020-08-12 16:32:37 +0900

    add PolyNet and InceptionResNetV2

---
+++ models/dim2/inception_resnet.py
@@ -0,0 +1,264 @@
+import torch
+import torch.nn as nn
+
+class InceptionResNetV2(nn.Module):
+    def __init__(self, in_shape, n_classes, dropout_rate=0.5):
+        super(InceptionResNetV2, self).__init__()
+        
+        self.in_shape = in_shape
+        in_channels = in_shape[0]
+
+        stage_a = IRV2StageA
+        stage_b = IRV2StageB
+        stage_c = IRV2StageC
+        self.ir_base = InceptionResNetV2Base(in_channels, stage_a, stage_b, stage_c)
+        self.aux = None
+
+        in_ch = self._calc_last_feature_channels()
+        self.classifier = nn.Sequential(
+            nn.AdaptiveAvgPool2d((1, 1)),
+            nn.Flatten(),
+            nn.Dropout(p=dropout_rate),
+            nn.Linear(in_ch, n_classes)
+        )
+    
+    def _calc_last_feature_channels(self):
+        dummy = torch.zeros([2]+list(self.in_shape), dtype=torch.float)
+        out = self.ir_base(dummy)
+        return out.size(1)
+
+    def forward(self, x):
+        x = self.ir_base(x)
+        x = self.classifier(x)
+        return x
+
+class InceptionResNetV2Base(nn.Module):
+    def __init__(self, in_channels, stage_a, stage_b, stage_c):
+        super(InceptionResNetV2Base, self).__init__()
+
+        self.head_path = nn.Sequential(
+            nn.Conv2d(in_channels, 32, kernel_size=3, stride=2, padding=3//2),
+            nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=3//2),
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=3//2),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 80, kernel_size=1, stride=1, padding=0),
+            nn.Conv2d(80, 192, kernel_size=3, stride=1, padding=3//2),
+            nn.MaxPool2d(kernel_size=3, stride=2)
+        )
+
+        # Branches 1
+        self.b1_conv11 = nn.Conv2d(192, 96, kernel_size=1, stride=1, padding=0)
+
+        self.b1_conv21 = nn.Conv2d(192, 48, kernel_size=1, stride=1, padding=0)
+        self.b1_conv22 = nn.Conv2d(48, 64, kernel_size=5, stride=1, padding=5//2)
+
+        self.b1_conv31 = nn.Conv2d(192, 64, kernel_size=1, stride=1, padding=0)
+        self.b1_conv32 = nn.Conv2d(64, 96, kernel_size=3, stride=1, padding=3//2)
+        self.b1_conv33 = nn.Conv2d(96, 96, kernel_size=3, stride=1, padding=3//2)
+
+        self.b1_avgpool41 = nn.AvgPool2d(kernel_size=3, stride=1, padding=3//2)
+        self.b1_conv42 = nn.Conv2d(192, 64, kernel_size=1, stride=1, padding=0)
+
+        self.stage_a_path = stage_a(320)
+
+        # Branches 2
+        self.b2_conv11 = nn.Conv2d(320, 384, kernel_size=3, stride=1, padding=3//2)
+
+        self.b2_conv21 = nn.Conv2d(320, 256, kernel_size=1, stride=1, padding=0)
+        self.b2_conv22 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=3//2)
+        self.b2_conv23 = nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=3//2)
+
+        self.b2_avgpool31 = nn.AvgPool2d(kernel_size=3, stride=1, padding=3//2)
+
+        self.stage_b_path = stage_b(1088)
+
+
+        # Branches 3
+        self.b3_conv11 = nn.Conv2d(1088, 256, kernel_size=1, stride=1, padding=0)
+        self.b3_conv12 = nn.Conv2d(256, 384, kernel_size=3, stride=2, padding=3//2)
+
+        self.b3_conv21 = nn.Conv2d(1088, 256, kernel_size=1, stride=1, padding=0)
+        self.b3_conv22 = nn.Conv2d(256, 288, kernel_size=3, stride=2, padding=3//2)
+ 
+        self.b3_conv31 = nn.Conv2d(1088, 256, kernel_size=1, stride=1, padding=0)
+        self.b3_conv32 = nn.Conv2d(256, 288, kernel_size=3, stride=1, padding=3//2)
+        self.b3_conv33 = nn.Conv2d(288, 320, kernel_size=3, stride=2, padding=3//2)
+
+        self.b3_maxpool31 = nn.MaxPool2d(kernel_size=3, stride=2, padding=3//2)
+
+        self.stage_c_path = stage_c(2080)
+        self.conv = nn.Conv2d(2080, 1536, kernel_size=1, stride=1, padding=0)
+
+
+    def forward(self, x):
+        x = self.head_path(x)
+
+        # Branches 1
+        b11 = self.b1_conv11(x)
+        b12 = self.b1_conv22(self.b1_conv21(x))
+        b13 = self.b1_conv33(self.b1_conv32(self.b1_conv31(x)))
+        b14 = self.b1_conv42(self.b1_avgpool41(x))
+        x = torch.cat([b11, b12, b13, b14], dim=1)
+
+        x = self.stage_a_path(x)
+
+        # Branches 2
+        b21 = self.b2_conv11(x)
+        b22 = self.b2_conv23(self.b2_conv22(self.b2_conv21(x)))
+        b23 = self.b2_avgpool31(x)
+        x = torch.cat([b21, b22, b23], dim=1)
+
+        x = self.stage_b_path(x)
+
+        # Branchex 3
+        b31 = self.b3_conv12(self.b3_conv11(x))
+        b32 = self.b3_conv22(self.b3_conv21(x))
+        b33 = self.b3_conv33(self.b3_conv32(self.b3_conv31(x)))
+        b34 = self.b3_maxpool31(x)
+        x = torch.cat([b31, b32, b33, b34], dim=1)
+
+        x = self.stage_c_path(x)
+
+        x = self.conv(x)
+
+        return x
+
+class IRV2StageA(nn.Module):
+    def __init__(self, in_channels):
+        super(IRV2StageA, self).__init__()
+        self.stage = nn.Sequential(
+            *[InceptionModule35(in_channels=in_channels, scale=0.17, activation_fn=nn.ReLU) for _ in range(10)]
+        )
+    
+    def forward(self, x):
+        return self.stage(x)
+
+class IRV2StageB(nn.Module):
+    def __init__(self, in_channels):
+        super(IRV2StageB, self).__init__()
+        self.stage = nn.Sequential(
+            *[InceptionModule17(in_channels=in_channels, scale=0.1, activation_fn=nn.ReLU) for _ in range(20)]
+        )
+    
+    def forward(self, x):
+        return self.stage(x)
+
+class IRV2StageC(nn.Module):
+    def __init__(self, in_channels):
+        super(IRV2StageC, self).__init__()
+        self.stage= nn.Sequential(
+            *[InceptionModule8(in_channels=in_channels, scale=0.2, activation_fn=nn.ReLU) for _ in range(9)]
+        )
+        self.block = InceptionModule8(in_channels=in_channels, scale=1.0, activation_fn=None)
+    
+    def forward(self, x):
+        x = self.stage(x)
+        x = self.block(x)
+        return x
+
+class InceptionModule35(nn.Module):
+    def __init__(self, in_channels, scale=1.0, activation_fn=nn.ReLU):
+        super(InceptionModule35, self).__init__()
+
+        self.scale = scale
+
+        self.conv11 = nn.Conv2d(in_channels, 32, kernel_size=1, stride=1, padding=0)
+
+        self.conv21 = nn.Conv2d(in_channels, 32, kernel_size=1, stride=1, padding=0)
+        self.conv22 = nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=3//2)
+
+        self.conv31 = nn.Conv2d(in_channels, 32, kernel_size=1, stride=1, padding=0)
+        self.conv32 = nn.Conv2d(32, 48, kernel_size=3, stride=1, padding=3//2)
+        self.conv33 = nn.Conv2d(48, 64, kernel_size=3, stride=1, padding=3//2)
+
+        self.conv_f = nn.Conv2d(32+32+64, in_channels, kernel_size=1, stride=1, padding=0)
+        if activation_fn is not None:
+            self.activation = activation_fn()
+        else:
+            self.activation = None
+    
+    def forward(self, x):
+        identity = x
+        b1 = self.conv11(x)
+        b2 = self.conv22(self.conv21(x))
+        b3 = self.conv33(self.conv32(self.conv31(x)))
+        x = torch.cat([b1, b2, b3], dim=1)
+        scaled_up = self.conv_f(x) * self.scale
+        x = identity + scaled_up
+        if self.activation is not None:
+            x = self.activation(x)
+
+        return x
+
+class InceptionModule17(nn.Module):
+    def __init__(self, in_channels, scale=1.0, activation_fn=nn.ReLU):
+        super(InceptionModule17, self).__init__()
+
+        self.scale = scale
+
+        self.conv11 = nn.Conv2d(in_channels, 192, kernel_size=1, stride=1, padding=0)
+
+        self.conv21 = nn.Conv2d(in_channels, 128, kernel_size=1, stride=1, padding=0)
+        self.conv22 = nn.Conv2d(128, 160, kernel_size=(1, 7), stride=1, padding=(0, 7//2))
+        self.conv23 = nn.Conv2d(160, 192, kernel_size=(7, 1), stride=1, padding=(7//2, 0))
+
+        self.conv_f = nn.Conv2d(192+192, in_channels, kernel_size=1, stride=1, padding=0)
+        if activation_fn is not None:
+            self.activation = activation_fn()
+        else:
+            self.activation = None
+    
+    def forward(self, x):
+        identity = x
+        b1 = self.conv11(x)
+        b2 = self.conv23(self.conv22(self.conv21(x)))
+        x = torch.cat([b1, b2], dim=1)
+        scaled_up = self.conv_f(x) * self.scale
+        x = identity + scaled_up
+        if self.activation is not None:
+            x = self.activation(x)
+
+        return x
+
+class InceptionModule8(nn.Module):
+    def __init__(self, in_channels, scale=1.0, activation_fn=nn.ReLU):
+        super(InceptionModule8, self).__init__()
+ 
+        self.scale = scale
+
+        self.conv11 = nn.Conv2d(in_channels, 192, kernel_size=1, stride=1, padding=0)
+
+        self.conv21 = nn.Conv2d(in_channels, 192, kernel_size=1, stride=1, padding=0)
+        self.conv22 = nn.Conv2d(192, 224, kernel_size=(1, 3), stride=1, padding=(0, 3//2))
+        self.conv23 = nn.Conv2d(224, 256, kernel_size=(3, 1), stride=1, padding=(3//2, 0))
+
+        self.conv_f = nn.Conv2d(192+256, in_channels, kernel_size=1, stride=1, padding=0)
+        if activation_fn is not None:
+            self.activation = activation_fn()
+        else:
+            self.activation = None
+    
+    def forward(self, x):
+        identity = x
+        b1 = self.conv11(x)
+        b2 = self.conv23(self.conv22(self.conv21(x)))
+        x = torch.cat([b1, b2], dim=1)
+        scaled_up = self.conv_f(x) * self.scale
+        x = identity + scaled_up
+        if self.activation is not None:
+            x = self.activation(x)
+
+        return x
+
+
+if __name__ == '__main__':
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    net = InceptionResNetV2(in_shape=(3, 100, 100), n_classes=10).to(device)
+    dummy = torch.zeros((50, 3, 100, 100), dtype=torch.float).to(device)
+
+    out = net(dummy)
+
+    print(out.size())
+
+ 
---
+++ models/dim2/polynet.py
@@ -0,0 +1,128 @@
+import torch
+import torch.nn as nn
+
+from inception_resnet import InceptionResNetV2Base, InceptionModule35, InceptionModule17, InceptionModule8
+
+class PolyNet(nn.Module):
+    def __init__(self, in_shape, n_classes, dropout_rate=0.5):
+        super(PolyNet, self).__init__()
+
+        self.in_shape = in_shape
+        in_channels = in_shape[0]
+
+        stage_a = PolyInceptionModule_stage_a
+        stage_b = PolyInceptionModule_stage_b
+        stage_c = PolyInceptionModule_stage_c
+        self.ir_base = InceptionResNetV2Base(in_channels, stage_a, stage_b, stage_c)
+        self.aux = None
+
+        in_ch = self._calc_last_feature_channels()
+        self.classifier = nn.Sequential(
+            nn.AdaptiveAvgPool2d((1, 1)),
+            nn.Flatten(),
+            nn.Dropout(p=dropout_rate),
+            nn.Linear(in_ch, n_classes)
+        )
+    
+    def _calc_last_feature_channels(self):
+        dummy = torch.zeros([2]+list(self.in_shape), dtype=torch.float)
+        out = self.ir_base(dummy)
+        return out.size(1)
+
+    def forward(self, x):
+        x = self.ir_base(x)
+        x = self.classifier(x)
+        return x
+
+class PolyInceptionModule_2way(nn.Module):
+    def __init__(self, module, is_activated=True, **kwargs):
+        super(PolyInceptionModule_2way, self).__init__()
+        self.mod1 = module(**kwargs)
+        self.mod2 = module(**kwargs)
+        if is_activated:
+            self.activation = nn.ReLU(inplace=True)
+        else:
+            self.activation = None
+    
+    def forward(self, x):
+        o1 = self.mod1(x)
+        o2 = self.mod2(x)
+        x = x + o1 + o2
+        if self.activation is not None:
+            x = self.activation(x)
+        
+        return x
+
+class PolyInceptionModule_poly3(nn.Module):
+    def __init__(self, module, is_activated=True, **kwargs):
+        super(PolyInceptionModule_poly3, self).__init__()
+        self.mod = module(**kwargs)
+        if is_activated:
+            self.activation = nn.ReLU(inplace=True)
+        else:
+            self.activation = None
+    
+    def forward(self, x):
+        o1 = self.mod(x)
+        o2 = self.mod(o1)
+        o3 = self.mod(o2)
+        x = x + o1 + o2 + o3 
+        if self.activation is not None:
+            x = self.activation(x)
+        
+        return x
+
+
+class PolyInceptionModule_stage_a(nn.Module):
+    def __init__(self, in_channels):
+        super(PolyInceptionModule_stage_a, self).__init__()
+        self.stage = nn.Sequential(
+            *[PolyInceptionModule_2way(InceptionModule35, in_channels=in_channels, scale=0.17, activation_fn=nn.ReLU) for _ in range(10)]
+        )
+    
+    def forward(self, x):
+        return self.stage(x)
+
+class PolyInceptionModule_stage_b(nn.Module):
+    def __init__(self, in_channels):
+        super(PolyInceptionModule_stage_b, self).__init__()
+        modules = []
+        for _ in range(10):
+            modules += [
+                PolyInceptionModule_poly3(InceptionModule17, in_channels=in_channels, scale=0.17, activation_fn=nn.ReLU),
+                PolyInceptionModule_2way(InceptionModule17, in_channels=in_channels, scale=0.17, activation_fn=nn.ReLU),
+            ]
+        self.stage = nn.Sequential(*modules)
+    
+    def forward(self, x):
+        return self.stage(x)
+
+class PolyInceptionModule_stage_c(nn.Module):
+    def __init__(self, in_channels):
+        super(PolyInceptionModule_stage_c, self).__init__()
+        modules = []
+        for _ in range(4):
+            modules += [
+                PolyInceptionModule_poly3(InceptionModule8, in_channels=in_channels, scale=0.2, activation_fn=nn.ReLU),
+                PolyInceptionModule_2way(InceptionModule8, in_channels=in_channels, scale=0.2, activation_fn=nn.ReLU),
+            ]
+        modules += [
+            PolyInceptionModule_poly3(InceptionModule8, in_channels=in_channels, scale=0.2, activation_fn=nn.ReLU),
+            PolyInceptionModule_2way(InceptionModule8, in_channels=in_channels, scale=1., activation_fn=None),
+        ]
+        self.stage = nn.Sequential(*modules)
+    
+    def forward(self, x):
+        return self.stage(x)
+
+if __name__ == '__main__':
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    net = PolyNet(in_shape=(3, 100, 100), n_classes=10).to(device)
+    dummy = torch.zeros((50, 3, 100, 100), dtype=torch.float).to(device)
+
+    out = net(dummy)
+
+    print(out.size())
+
+ 
